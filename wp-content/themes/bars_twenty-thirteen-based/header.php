eda8491786ca563fa97c615f9816e475739532f8
Andres Mata Suarez <amatasuarez@gmail.com>
2013-09-30 19:31:10 -0300

    Limpieza

--- wp-content/themes/bars_twenty-thirteen-based/header.php
+++ wp-content/themes/bars_twenty-thirteen-based/header.php
@@ -28,7 +28,8 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
-	<script type="text/javascript" src="/js/bars.js"></script>
+	
+	<script type="text/javascript" src="<? echo get_stylesheet_directory_uri();?>/js/bars.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
--- wp-content/themes/bars_twenty-twelve-based/searchform.php
+++
@@ -1,4 +0,0 @@
-<form id="searchform" method="get" action="<?php bloginfo('siteurl')?>/">
-	<input type="text" name="s" id="s" class="textbox" value="<?php echo wp_specialchars($s, 1); ?>" />
-	<input id="btnSearch" type="submit" name="submit" value="<?php _e('Go'); ?>" />
-</form>
