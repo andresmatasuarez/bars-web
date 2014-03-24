6722e02ea37798f5e24c8408ff555895587c91e7
Andres Mata Suarez <amatasuarez@gmail.com>
2014-03-24 11:04:49 -0300

    Probando la validacion del form

--- wp-content/themes/bars2013/contact.php
+++ wp-content/themes/bars2013/contact.php
@@ -17,63 +17,64 @@
 							Por cualquier duda, consulta o si representás a un medio de prensa, dejanos tu mensaje!
 						</p>
 						
-						<form class="contact-form" name="contact" method="post">
-							<div class="contact-form-input contact-form-input-name">
-								<label class="required" for="name" >Nombre</label>
-								<input type="text" name="name" id="name" value="" required/>
-								<span class="contact-form-input-hint">
-									45 caracteres restantes.
-								</span>
-							</div>	
-							<div class="contact-form-input contact-form-input-email">
-								<label class="required" for="email">Email</label>
-								<input type="text" name="email" id="email"  value="" required/>
-								<span class="contact-form-input-hint">
-									45 caracteres restantes.
-								</span>
-							</div>
-							
-							<div class="clear"></div>
-							
-							<div class="contact-form-input contact-form-input-media"> 
-								<label for="media">Medio de prensa</label>
-								<input type="text" name="media" id="media"  value="" />
-								<span class="contact-form-input-hint">
-									45 caracteres restantes.
-								</span>
-							</div>
-							
-							<div class="contact-form-input contact-form-input-media"> 
-								<label for="media">Medio de prensa</label>
-								<input type="text" name="media" id="media"  value="" />
-								<span class="contact-form-input-hint">
-									45 caracteres restantes.
-								</span>
-							</div>
-							
-							<div class="contact-form-input contact-form-input-subject">
-								<label class="required" for="subject">Asunto</label>
-								<input type="text" name="subject" id="subject"  value="" />
-								<span class="contact-form-input-hint">
-									100 caracteres restantes.
-								</span>
-							</div>
-							<div class="contact-form-input contact-form-input-message">
-								<label class="required" for="message">Message</label>
-								<textarea name="message" id="message" required></textarea>
-								<span class="contact-form-input-hint">
-									255 caracteres restantes.
-								</span>
-							</div>
-							<div class="contact-form-input contact-form-input-captcha">
-								<label class="required" for="captcha">Name the small house pet that says "<i>meow</i>"</label>
-								<input type="text" name="captcha" value="" required/>
-								<span class="contact-form-input-hint"></span>
-							</div>
-							<div class="contact-form-input">
-								<input id="submit" type="submit" name="submit" value="Send" />
-							</div>
+						<form class="contact-form" id="contact-form" method="post">
+							<fieldset>
+								<div class="contact-form-input contact-form-input-name">
+									<label class="required" for="name" >Nombre</label>
+									<input type="text" name="name" id="name" value=""/>
+									<span class="contact-form-input-hint">
+										45 caracteres restantes.
+									</span>
+								</div>	
+								<div class="contact-form-input contact-form-input-email">
+									<label class="required" for="email">Email</label>
+									<input type="text" name="email" id="email"  value="" />
+									<span class="contact-form-input-hint">
+										45 caracteres restantes.
+									</span>
+								</div>
 								
+								<div class="clear"></div>
+								
+								<div class="contact-form-input contact-form-input-media"> 
+									<label for="media">Medio de prensa</label>
+									<input type="text" name="media" id="media"  value="" />
+									<span class="contact-form-input-hint">
+										45 caracteres restantes.
+									</span>
+								</div>
+								
+								<div class="contact-form-input contact-form-input-media"> 
+									<label for="media">Medio de prensa</label>
+									<input type="text" name="media" id="media"  value="" />
+									<span class="contact-form-input-hint">
+										45 caracteres restantes.
+									</span>
+								</div>
+								
+								<div class="contact-form-input contact-form-input-subject">
+									<label class="required" for="subject">Asunto</label>
+									<input type="text" name="subject" id="subject"  value="" />
+									<span class="contact-form-input-hint">
+										100 caracteres restantes.
+									</span>
+								</div>
+								<div class="contact-form-input contact-form-input-message">
+									<label class="required" for="message">Message</label>
+									<textarea name="message" id="message" ></textarea>
+									<span class="contact-form-input-hint">
+										255 caracteres restantes.
+									</span>
+								</div>
+								<div class="contact-form-input contact-form-input-captcha">
+									<label class="required" for="captcha">Name the small house pet that says "<i>meow</i>"</label>
+									<input type="text" name="captcha" value="" />
+									<span class="contact-form-input-hint"></span>
+								</div>
+								<div class="contact-form-input">
+									<input id="submit" type="submit" name="submit" value="Send" />
+								</div>								
+							</fieldset>
 						</form>
 						
 						<div id="success">
---
+++ wp-content/themes/bars2013/process.php
@@ -0,0 +1,26 @@
+<?php
+ 
+	// Receiver email
+    $to = 'amatasuarez@gmail.com';
+	
+	// Email headers & subject
+    $headers = 'From: $from';
+    $subject = "BARS / Contact form";
+	
+	// Form input
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $media = $_REQUEST['media'];
+    $media = $_REQUEST['subject'];
+    $media = $_REQUEST['message'];
+ 
+    $fields = { 'name', 'email', 'media', 'subject', 'message' };
+ 
+    $body = "Here is what was sent:\n\n";
+	foreach($fields as $field){
+		$body = $body . '\n' . $field . $_REQUEST[$field];
+	}
+ 
+    $send = mail($to, $subject, $body, $headers);
+ 
+?>
