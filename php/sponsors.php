4c7efa093e76b39c4f6546f2ba6348d4fd90571a
Andres Mata Suarez <amatasuarez@gmail.com>
2014-10-22 01:27:14 -0300

    Updated spnosors for bars15

--- php/sponsors.php
+++ php/sponsors.php
@@ -22,11 +22,23 @@
 						<div class="auspiciante auspiciante_videoflims">
 							<a href="http://www.videoflims.com.ar/" target="_blank" alt="Video Flims" ></a>
 						</div>
-						<div class="auspiciante auspiciante_incaa">
-							<a href="http://www.incaa.gov.ar/" target="_blank" alt="INCAA" ></a>
-						</div>
 						<div class="auspiciante auspiciante_findlingfilms">
 							<a href="http://findlingfilms.com/" target="_blank" alt="Findling Films" ></a>
+						</div>
+					</div>
+
+					<div class="page-header">
+						Patrocinan
+					</div>
+
+					<div class="clear scratch"></div>
+
+					<div class="sponsors" >
+						<div class="auspiciante auspiciante_vans">
+							<a href="https://www.facebook.com/vans.arg?fref=ts" target="_blank" alt="Vans 'Off The Wall'" ></a>
+						</div>
+						<div class="auspiciante auspiciante_incaa">
+							<a href="http://www.incaa.gov.ar/" target="_blank" alt="INCAA" ></a>
 						</div>
 					</div>
 
@@ -37,38 +49,17 @@
 					<div class="clear scratch"></div>
 
 					<div class="sponsors" >
+						<div class="auspiciante auspiciante_bsasciudad">
+							<a href="http://www.buenosaires.gob.ar/" target="_blank" alt="Buenos Aires Ciudad" ></a>
+						</div>
 						<div class="auspiciante auspiciante_monumental">
 							<a href="http://www.cinesmultiplex.com.ar/" target="_blank" alt="Monumental Lavalle" ></a>
 						</div>
 						<div class="auspiciante auspiciante_cunnington">
 							<a href="http://www.cunnington.com.ar/" target="_blank" alt="Cunnington" ></a>
 						</div>
-						<div class="auspiciante auspiciante_mostaza">
-							<a href="http://www.mostazaweb.com.ar/" target="_blank" alt="Mostaza Hamburguesas" ></a>
-						</div>
-						<div class="auspiciante auspiciante_campari">
-							<a href="http://www.campari.com/ar/es/" target="_blank" alt="Campari Argentina" ></a>
-						</div>
-					</div>
-
-					<div class="page-header sponsors-header">
-						Difunden
-					</div>
-
-					<div class="clear scratch"></div>
-
-					<div class="sponsors">
-						<div class="auspiciante auspiciante_syfy">
-							<a href="http://www.syfyla.com/es" target="_blank" alt="SyFy" ></a>
-						</div>
-						<div class="auspiciante auspiciante_cinesargentinos">
-							<a href="http://www.cinesargentinos.com.ar/" target="_blank" alt="Cines Argentinos" ></a>
-						</div>
-						<div class="auspiciante auspiciante_nacionalrock">
-							<a href="http://nacionalrock.com/" target="_blank" alt="Nacional Rock" ></a>
-						</div>
-						<div class="auspiciante auspiciante_bsasciudad">
-							<a href="http://www.buenosaires.gob.ar/" target="_blank" alt="Buenos Aires Ciudad" ></a>
+						<div class="auspiciante auspiciante_grolsch">
+							<!-- <a href="" target="_blank" alt="Grolsch Lager" ></a> -->
 						</div>
 					</div>
 
@@ -78,18 +69,42 @@
 
 					<div class="clear scratch"></div>
 
-					<div class="sponsors" >
-						<div class="auspiciante auspiciante_flaviogrecopaglia">
-							<a href="https://www.behance.net/flaviogreco" target="_blank" alt="Flavio Greco Paglia Dibujante - Ilustrador" ></a>
+					<div class="sponsors">
+						<div class="auspiciante auspiciante_cinesargentinos">
+							<a href="http://www.cinesargentinos.com.ar/" target="_blank" alt="Cines Argentinos" ></a>
+						</div>
+						<div class="auspiciante auspiciante_nacionalrock">
+							<a href="http://nacionalrock.com/" target="_blank" alt="Nacional Rock" ></a>
 						</div>
 						<div class="auspiciante auspiciante_illusiondreamsfx">
 							<a href="https://www.facebook.com/pages/ilusiondreams-fx/185415858195529" target="_blank" alt="Illusion Dreams FX" ></a>
 						</div>
-						<div class="auspiciante auspiciante_filmoteca">
-							<a href="http://filmotecabuenosaires.wordpress.com/" target="_blank" alt="Filmoteca Buenos Aires" ></a>
-						</div>
 						<div class="auspiciante auspiciante_lacripta">
 							<!-- <a href="" target="_blank" alt="Salas La Cripta" ></a> -->
+						</div>
+						<div class="auspiciante auspiciante_festeringslime">
+							<a href="mailto:amatasuarez@gmail.com" target="_blank" alt="Festering Slime Web Design" ></a>
+						</div>
+						<div class="auspiciante auspiciante_rafma">
+							<a href="https://www.facebook.com/pages/RAFMA-Red-Argentina-de-Festivales-y-Muestras-Audiovisuales/151718504869922" target="_blank" alt="RAFMA Red Argentina de Festivales y Muestras Audiovisuales" ></a>
+						</div>
+						<div class="auspiciante auspiciante_leotripi">
+							<!-- <a href="" target="_blank" alt="Toronja Producciones" ></a> -->
+						</div>
+						<div class="auspiciante auspiciante_ab">
+							<a href="http://arianabouzon.wix.com/arianabouzon#!" target="_blank" alt="AB Producciones" ></a>
+						</div>
+						<div class="auspiciante auspiciante_toronja">
+							<a href="http://www.toronjaproducciones.com.ar/" target="_blank" alt="Toronja Producciones" ></a>
+						</div>
+						<div class="auspiciante auspiciante_vacomoloco">
+							<a href="https://www.facebook.com/vacomoloco" target="_blank" alt="Vacomoloco" ></a>
+						</div>
+						<div class="auspiciante auspiciante_planetarycomics">
+							<a href="http://planetary-comics.blogspot.com.ar/" target="_blank" alt="Planetary Comics" ></a>
+						</div>
+						<div class="auspiciante auspiciante_requiem">
+							<a href="https://www.facebook.com/pages/Requiem/56147413104" target="_blank" alt="Requiem club" ></a>
 						</div>
 						<div class="auspiciante auspiciante_piromaniafx">
 							<a href="http://www.piromaniafx.com.ar/‎" target="_blank" alt="Piromania FX" ></a>
@@ -97,48 +112,33 @@
 						<div class="auspiciante auspiciante_rentalbsas">
 							<a href="http://www.rental-bsas.com.ar/" target="_blank" alt="Rental Bs. As." ></a>
 						</div>
-						<div class="auspiciante auspiciante_planetarycomics">
-							<a href="http://planetary-comics.blogspot.com.ar/" target="_blank" alt="Planetary Comics" ></a>
-						</div>
 						<div class="auspiciante auspiciante_eda">
 							<a href="http://www.edaeditores.com.ar/" target="_blank" alt="EDA" ></a>
 						</div>
-						<div class="auspiciante auspiciante_toronja">
-							<a href="http://www.toronjaproducciones.com.ar/" target="_blank" alt="Toronja Producciones" ></a>
-						</div>
-						<div class="auspiciante auspiciante_festeringslime">
-							<a href="mailto:amatasuarez@gmail.com" target="_blank" alt="Festering Slime Web Design" ></a>
-						</div>
-						<div class="auspiciante auspiciante_requiem">
-							<a href="https://www.facebook.com/pages/Requiem/56147413104" target="_blank" alt="Requiem club" ></a>
-						</div>
-						<div class="auspiciante auspiciante_ticoral">
-							<a href="http://www.ticoral.com" target="_blank" alt="Ticoral" ></a>
+						<div class="auspiciante auspiciante_flaviogrecopaglia">
+							<a href="https://www.behance.net/flaviogreco" target="_blank" alt="Flavio Greco Paglia Dibujante - Ilustrador" ></a>
 						</div>
 						<div class="auspiciante auspiciante_cinefania">
 							<a href="http://www.cinefania.com/" target="_blank" alt="Cinefanía" ></a>
 						</div>
-						<div class="auspiciante auspiciante_vacomoloco">
-							<a href="https://www.facebook.com/vacomoloco" target="_blank" alt="Vacomoloco" ></a>
+						<div class="auspiciante auspiciante_ticoral">
+							<a href="http://www.ticoral.com" target="_blank" alt="Ticoral" ></a>
 						</div>
-						<div class="auspiciante auspiciante_rafma">
-							<a href="https://www.facebook.com/pages/RAFMA-Red-Argentina-de-Festivales-y-Muestras-Audiovisuales/151718504869922" target="_blank" alt="RAFMA Red Argentina de Festivales y Muestras Audiovisuales" ></a>
+						<div class="auspiciante auspiciante_wancamp">
+							<a href="http://www.wancamp.com.ar" target="_blank" alt="Wancamp" ></a>
 						</div>
-						<div class="auspiciante auspiciante_rarovhs">
-							<a href="http://www.rarovhs.com.ar/" target="_blank" alt="Raro VHS" ></a>
+						<div class="auspiciante auspiciante_miradadistribution">
+							<a href="https://www.facebook.com/MiradaDistribution?fref=ts" target="_blank" alt="Mirada Distribution" ></a>
 						</div>
-						<div class="auspiciante auspiciante_ab">
-							<a href="http://arianabouzon.wix.com/arianabouzon#!" target="_blank" alt="AB Producciones" ></a>
+						<div class="auspiciante auspiciante_uip">
+							<a href="https://www.facebook.com/UIParg?fref=ts" target="_blank" alt="UIP" ></a>
 						</div>
-						<div class="auspiciante auspiciante_leotripi">
-							<!-- <a href="" target="_blank" alt="Toronja Producciones" ></a> -->
+						<div class="auspiciante auspiciante_cdi">
+							<a href="https://www.facebook.com/CDIfilms?fref=ts" target="_blank" alt="CDI" ></a>
 						</div>
-
-						<!--
-						<div class="auspiciante auspiciante_fantafestivales">
-							<a href="http://www.fantafestivales.org/" target="_blank" alt="Fanta Festivales" ></a>
+						<div class="auspiciante auspiciante_warnerbrosargentina">
+							<a href="https://www.facebook.com/warnerbrospicturesargentina" target="_blank" alt="Warner Bros Argentina" ></a>
 						</div>
-						-->
 
 					</div>
 				</div>
