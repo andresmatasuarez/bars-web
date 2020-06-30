0a047702be2021ebcd45c90b8e886fbd26a5bebd
Andrés Mata Suárez <amatasuarez@gmail.com>
2020-06-30 17:24:29 -0300

    Added new BARS 'about' text

--- php/festival.php
+++ php/festival.php
@@ -6,45 +6,51 @@
  * @subpackage bars2013
  */
 	get_header();
+
+	// Get paragraph separator from SVG file
+	$paragraphSeparator = file_get_contents(dirname(__FILE__) . '/paragraph-separator.svg');
+	$position = strpos($paragraphSeparator, '<svg');
+	$paragraphSeparator = substr($paragraphSeparator, $position);
 ?>
 				<div id="page-festival" class="page">
 					<div class="page-header">
 						Acerca del festival
 					</div>
-					
+
 					<div class="scratch"></div>
-					
-					<div class="festival-history text-opensans" >
-						<p>El Buenos Aires Rojo Sangre es el único festival especializado en cine fantástico y bizarro que se hace en Argentina. Es una muestra orientada básicamente a producciones independientes y de bajo presupuesto.</p>
 
-						<p>Su concreción surgió naturalmente, a partir de la creciente realización en Argentina de films de estos géneros. Generalmente realizados en video y en forma ultraindependiente, estas películas no lograban hacerse un lugar en los principales festivales cinematográficos locales. Por eso los realizadores de muy interesantes largos, medios y cortometrajes debían conformarse con unas pocas proyecciones autogestionadas. Esto es porque el fantástico es aún considerado en ciertos círculos como un “género menor”.</p>
+					<div class="festival-history text-opensans">
+						<p><strong>Buenos Aires Rojo Sangre</strong> es el más antiguo festival de cine fantástico de Latinoamérica. Con un recorrido de dos décadas, el <strong>BARS</strong> es un espacio de encuentro y exhibición de cine fantástico de todo el mundo y un punto de resistencia para los creadores argentinos de cine de género. Nacido en el año 2000 como una muestra de cine independiente y, desde 2004, con el formato de festival competitivo, el <strong>BARS</strong> es una cita imprescindible para los amantes del género y para el público que quiere entrar en contacto con un cine que no se ve en otros festivales.</p>
 
-						<p>Por eso se hizo imperiosa la necesidad de reunir bajo un mismo ámbito a películas con las mismas problemáticas de distribución, para que pudieran acercarse a un público común.</p>
+						<h3 style="margin-top: 2.5em;">Buenos Aires Rojo Sangre 20 años</h3>
+						<p>
+							En sus 20 años de historia, el festival <strong>Buenos Aires Rojo Sangre</strong> acompañó y vio crecer a una generación de cineastas que, como nunca antes en la historia del cine argentino, impulsaron con fuerza una producción nacional de cine de terror, ciencia ficción y fantasía. En el festival se pudieron ver –muchas veces como estreno– obras de los referentes actuales de nuestro cine de género como <strong>Daniel de la Vega</strong>, <strong>Demian Rugna</strong>, <strong>Pablo Pares</strong>, <strong>Fabián Forte</strong>, <strong>Gabriel Grieco</strong>, <strong>Nicolás</strong> y <strong>Luciano Onetti</strong>, <strong>Gonzalo Calzada</strong>, <strong>Nicanor Loreti</strong> y muchísimos más.
+						</p>
 
-						<p>La primera edición del BARS se llevó a cabo en diciembre de 2000 en el auditorio de la Facultad de Ciencias Sociales de Universidad de Buenos Aires. Si bien fue un espacio limitado, sirvió como punto de partida que año tras año iría aumentando. Se pudieron ver películas como Plaga Zombie (Hernán Sáez y Pablo Parés) o El Planeta de los Hippies (Ernesto Aguilar) y varios cortometrajes.</p>
+						<p>Pero tampoco el festival dejó de lado la historia del fantástico nacional, recuperando films olvidados como <i>El Inquisidor</i> (Bernardo Arias) y <i>Seis pasajes al infierno</i> (Fernando Siro), estrenando en Buenos Aires films históricos como <i>El hombre bestia</i> (Camilo Zacaría Soprani), haciendo proyecciones en fílmico de clásicos como <i>Lo que vendrá</i> (Gustavo Mosquera R.), <i>Nazareno Cruz y el lobo</i> (Leonardo Favio) o <i>La venganza del sexo</i> (Emilio Vieyra) y hasta reestrenando clásicas obras de la TV argentina como <i>Mañana puede ser verdad</i> de Narciso Ibáñez Menta.</p>
 
-						<p>Para el 2001 se cambió de escenario. El Centro Cultural San Martín fue un importante marco para el crecimiento del festival. Con poquísima promoción se logró atraer a más de 600 espectadores, destacándose films como Fanáticos (Mariano Cattaneo e Ygnacio Cervio) y Nunca Asistas a este tipo de Fiestas (Hernán Sáez y Pablo Parés).</p>
+						<div class="paragraph-separator">
+							<?php echo $paragraphSeparator; ?>
+						</div>
 
-						<p>El 2002 fue el salto más importante del festival, ampliando temáticamente la programación y logrando una notable repercusión en la prensa y el público, hasta el punto de que en el mismo espacio que la edición anterior se logró triplicar el número de espectadores, llegando a superar los 1800. Entre los films proyectados se encuentran Plaga Zombie: Zona Mutante (Hernán Sáez y Pablo Parés) y Attack of the Killer Hog (Agustín Cavalieri y Marcos Meroni).</p>
+						<p>Iniciado como una pequeña muestra en 2000 para 50 personas en el microcine de la Facultad de Ciencias Sociales de la UBA posteriormente fue pasando por el Centro Cultural General San Martín, el Complejo Tita Merello, el Multiplex Lavalle hasta su última sede, el Multiplex Belgrano donde más de 10000 espectadores se acercaron a las diversas actividades, que incluyen proyecciones, charlas y talleres.</p>
 
-						<p>En el 2003 llegó la definitiva consolidación del festival, estrenándose siete largometrajes locales, -Run Run Bunny! (Mad Crampi), Tico tico (Marcelo Domizi), Baño de Sangre (Paula Pollacchi), Mala Carne (Fabián Forte), Ruta hacia la muerte (Francisco de Lezica) y Vacaciones en la tierra (Sebastián de Caro)- y preestrenándose otros tantos largometrajes de países como EE.UU., Japón, España, Francia e Italia. Se proyectaron más de 80 cortometrajes y las secciones retrospectivas estuvieron pobladas de clásicos en 35mm. Se destacó especialmente la recuperación del film argentino El Hombre Bestia (Camilo Zaccaría Soprani, 1934), la primer película fantástica local, que nunca se había estrenado en Buenos Aires. Casi 5000 espectadores demostraron el innegable crecimiento del festival.</p>
+						<p>Además de ser un espacio para difusión del cine local, también es un espacio para conocer el cine de género todo el mundo y a sus realizadores, trayendo figuras como <strong>Fede Álvarez</strong> (<i>Evil Dead</i>, <i>Don't breathe</i>), <strong>Mick Garris</strong> (<i>Masters of Horror</i>, <i>Sleepwalkers</i>) o <strong>Ruggero Deodato</strong> (<i>Cannibal Holocaust</i>, <i>The House on the Edge of the Park</i>).</p>
 
-						<p>Para la quinta edición (2004) se sumaron secciones competitivas tanto para corto como para largometrajes y se utilizaron las 3 salas del complejo Tita Merello en simultáneo, triplicando la cantidad de horas de proyección.</p>
+						<p>El <strong>BARS</strong> es también, durante todo el año, un espacio de difusión permanente del cine de género, organizando talleres para jóvenes de efectos especiales y realización de cine (Feria del libro infantil y juvenil), concursos especiales (Fin de semana sangriento), ciclos de proyecciones (Centro Cultural Recoleta), programas de tv (Ciudad Abierta, INCAATV). También hace 10 años el <strong>BARS</strong> organiza el <strong>Mendoza Rojo Sangre</strong>, una muestra anual con lo más destacado de cada edición del festival.</p>
 
-						<p>A partir del año 2008 el BARS se muda nuevamente, esta vez a la calle peatonal Lavalle, al cine Monumental, copando el subsuelo con sus dos salas.</p>
+						<br />
 
-						<p>El año 2009 fue de suma importancia para el festival, ya que no sólo fue su décimo aniversario, sino que también obtuvo récord de público con más de 7.000 tickets vendidos (aproximadamente 12.000 visitantes!). Se preestrenaron 4 largometrajes de circuito industrial: “Aparecidos”, “Terror en la Antártida”, “Cabeza de pescado” y “Lo siniestro”. Integraron la programación 11 películas argentinas; destacadas figuras conformaron el Jurado (Gustavo Mosquera, Cristian Bernard, Federico Wiemeyer y Gabriel Patrono entre otros). Se llevó a cabo el primer Zombiefest (convocatoria a cortos realizados durante la Zombiewalk -caminata zombie-) y se estrenó un documental de 95 minutos relatando la trayectoria de cine de género local en los primeros 10 años de festival.</p>
-
-						<p>El año 2010, superó a las ediciones anteriores en público, batiendo un nuevo récord de espectadores. El jurado también contó con diversos e importantes representantes del medio (Horacio Maldonado, Diego Trerotola, Javier Diment, Leo González). En el segundo aniversario de la patria, se reestrenó en 35mm “La sonámbula” de Fernando Spiner, película rodada en 1997, que se atrevió a imaginar a la Argentina del Bicentenario. Además la programación contó con 12 largometrajes Argentinos, 3 pre-estrenos internacionales y más de 130 cortos nacionales y del exterior.</p>
-
-						<p>En el año 2011, se celebró la doceava edición y nuevamente el récord de público fue batido (más de 10.000 tickets emitidos!). Se sumó una tercer sala del Complejo Monumental Lavalle, con lo que la programación aumentó significativamente. Estrenos de todas partes del mundo, Bolivia, Guatemala, Israel, Croacia, Costa Rica, Japón, Italia y preestrenos de títulos como “Attack the Block”, la francesa “Livide”, “La Profecía del 11-11-11″ y la nacional “Plaga Zombie: Zona Mutante: Revolución Tóxica” dando cierre a la más importante trilogía local. 17 películas Argentinas, por lo que una nueva sección “Panorama Nacional” fue inaugurada. Nuevamente alrededor de 120 cortometrajes en Competencia y en las ya clásicas funciones temáticas: zombies, bizarro, psicho killers, clase Z, etc. Conformaron el jurado Nahuel Pérez Bizcayart, Mariano Cattaneo, Sebastián “Berta” Muñiz, Sebatián Rotstein, y la grata presencia internacional de los directores del festival hermano Fantaspoa. Se llevaron a cabo 8 charlas temáticas, entre ellas la presentación de dos libros de FanEdiciones y una masterclass dictada por Fede Álvarez, director uruguayo quien dirigirá la nueva Evil Dead, apadrinado por Sam Raimi. Los 7 días de duración del festival fueron variados e intensos, demostrando el gran crecimiento perpetrado por el Festival a través de sus 12 años de trayectoria.</p>
-
-						<p>Este año, tendrá lugar la 13va edición! En el año 2012, el fin del mundo, tendremos un festival apocalíptico y lleno de sorpresas para todos los gustos!</p>
-
-						<p>El BARS fue declarado de interés por el Instituto Nacional de Cinematografía y Artes Audiovisuales, auspiciado por el Museo del Cine de la Ciudad de Buenos Aires y declarado de Interés Cultural por la Legislatura de la Ciudad Autónoma de Buenos Aires. Estas instituciones han comprendido que este festival es el único espacio para un tipo de cine que tradicionalmente ha sido olvidado. Indudablemente el Buenos Aires Rojo Sangre, un festival especializado que ya se ha convertido en un referente para la producción nacional de ciencia ficción, terror y fantasía.</p>
-					</div>
+						<a href="http://www.festivalrojosangre.com.ar">www.festivalrojosangre.com.ar</a>
+						<br />
+						Facebook: <a href="https://www.facebook.com/BuenosAiresRojoSangre" target="_blank" rel="noopener">BuenosAiresRojoSangre</a>
+						<br />
+						Twitter: <a href="https://twitter.com/rojosangre">@rojosangre</a>
+						<br />
+						Instagram: <a href="https://www.instagram.com/festivalrojosangre">@festivalrojosangre</a>
+					<div>
 				</div>
-				
+
 	<?php
 		get_sidebar();
 		get_footer();
