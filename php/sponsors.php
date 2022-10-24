071b2bbf6233249e0e9c8fb76ac274371c5ea338
Andrés Mata Suárez <amatasuarez@gmail.com>
2022-10-24 19:55:43 -0300

    Sponsors data is now loaded from editions.json

--- php/sponsors.php
+++ php/sponsors.php
@@ -5,226 +5,57 @@
  * @package WordPress
  * @subpackage bars2013
  */
+	require_once 'helpers.php';
+	require_once 'elements.php';
+	require_once 'editions.php';
+
 	get_header();
+
+	$latestEdition = Editions::current();
 ?>
 				<div id="page-sponsors" class="page">
 
-					<div class="page-header">
-						Organizan
-					</div>
+					<?php
+						$noSponsorsDefined = !isset($latestEdition['sponsors']) || empty($latestEdition['sponsors']);
 
-					<div class="clear scratch"></div>
+						if ($noSponsorsDefined) {
+							$linkToContact = get_permalink(get_page_by_path('contacto'));
+							renderWarningMessage(
+							  'Los auspiciantes para esta <br /> edición del festival no están disponibles',
+							  "Volvé a intentar más tarde o <br /> comunicate con nosotros usando el <a href=\"{$linkToContact}\">formulario de contacto</a>."
+							);
+						} else {
+							foreach($latestEdition['sponsors'] as $section) {
+					?>
+								<div class="page-header">
+									<?php echo $section['title'] ?>
+								</div>
 
-					<div class="sponsors" >
-						<div class="sponsor sponsor-quintadimension">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.quintadimension.com/" alt="Quinta Dimensión" ></a>
-						</div>
-						<div class="sponsor sponsor-findlingfilms">
-							<a target="_blank" rel="noopener noreferrer" href="http://findlingfilms.com/" alt="Findling Films" ></a>
-						</div>
-					</div>
+								<div class="clear scratch"></div>
 
-					<div class="page-header">
-						Auspician
-					</div>
-
-					<div class="clear scratch"></div>
-
-					<div class="sponsors" >
-						<div class="sponsor sponsor-incaa">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.incaa.gov.ar/" alt="INCAA" ></a>
-						</div>
-						<div class="sponsor sponsor-multiplex">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.cinesmultiplex.com.ar/" alt="Cines Multiplex" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-illusiondreamsfx">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/pages/ilusiondreams-fx/185415858195529" alt="Illusion Dreams FX" ></a>
-						</div>
-						<div class="sponsor sponsor-cunnington">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.cunnington.com.ar/" alt="Cunnington" ></a>
-						</div>
- 						<div class="sponsor sponsor-museo-sarmiento">
-							<a target="_blank" rel="noopener noreferrer" href="https://museosarmiento.cultura.gob.ar" alt="Museo Histórico Sarmiento" ></a>
-						</div>
-						-->
- 						<!-- <div class="sponsor sponsor-contar">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.cont.ar" alt="Contar" ></a>
-						</div>
- 						<div class="sponsor sponsor-flixxo">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.flixxo.com" alt="Flixxo" ></a>
-						</div>
-						<div class="sponsor sponsor-ministerio-de-cultura"></div>
-						-->
- 						<div class="sponsor sponsor-drag-clash">
-							<a target="_blank" rel="noopener noreferrer" href="http://youtube.com/dragclash" alt="Drag Clash" ></a>
-						</div>
- 						<div class="sponsor sponsor-itau-fundacion">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.fundacionitau.org.ar/" alt="Itaú Fundación" ></a>
-						</div>
- 						<div class="sponsor sponsor-ba-audiovisual">
-							<a target="_blank" rel="noopener noreferrer" href="https://vivamoscultura.buenosaires.gob.ar/" alt="BA Audiovisual" ></a>
-						</div>
- 						<div class="sponsor sponsor-vivamos-cultura">
-							<a target="_blank" rel="noopener noreferrer" href="https://vivamoscultura.buenosaires.gob.ar/" alt="Vivamos Cultura" ></a>
-						</div>
- 						<div class="sponsor sponsor-museos-ba">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.buenosaires.gob.ar/cultura/museos" alt="Museos BA" ></a>
-						</div>
-						<div class="sponsor sponsor-bsasciudad">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.buenosaires.gob.ar/" alt="Buenos Aires Ciudad" ></a>
-						</div>
-						<div class="sponsor sponsor-mecenazgo-cultural"></div>
-					</div>
-
-					<div class="page-header sponsors-header">
-						Colaboran
-					</div>
-
-					<div class="clear scratch"></div>
-
-					<div class="sponsors">
-						<div class="sponsor sponsor-lacripta">
-							<a target="_blank" rel="noopener noreferrer" href="http://reservas.salaslacripta.com.ar/" alt="Salas La Cripta" ></a>
-						</div>
-						<div class="sponsor sponsor-rafma">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/pages/RAFMA-Red-Argentina-de-Festivales-y-Muestras-Audiovisuales/151718504869922" alt="RAFMA Red Argentina de Festivales y Muestras Audiovisuales" ></a>
-						</div>
-						<div class="sponsor sponsor-ab">
-							<a target="_blank" rel="noopener noreferrer" href="http://arianabouzon.wix.com/arianabouzon#!" alt="AB Producciones" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-toronja">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.toronjaproducciones.com.ar/" alt="Toronja Producciones" ></a>
-						</div>
-						<div class="sponsor sponsor-requiem">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/pages/Requiem/56147413104" alt="Requiem club" ></a>
-						</div>
-						-->
-						<div class="sponsor sponsor-piromaniafx">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.piromaniafx.com.ar/‎" alt="Piromania FX" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-rentalbsas">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.rental-bsas.com.ar/" alt="Rental Bs. As." ></a>
-						</div>
-						-->
-						<div class="sponsor sponsor-eda">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.edaeditores.com.ar/" alt="EDA" ></a>
-						</div>
-						<div class="sponsor sponsor-cinefania">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.cinefania.com/" alt="Cinefanía" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-wancamp">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.wancamp.com.ar" alt="Wancamp" ></a>
-						</div>
-						<div class="sponsor sponsor-ticoral">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.ticoral.com" alt="Ticoral" ></a>
-						</div>
-						-->
-						<div class="sponsor sponsor-flaviogrecopaglia">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.behance.net/flaviogreco" alt="Flavio Greco Paglia Dibujante - Ilustrador" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-aclf">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Academiadecine" alt="ACLF" ></a>
-						</div>
-						-->
-						<div class="sponsor sponsor-apima">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.apima.com.ar" alt="APIMA" ></a>
-						</div>
-						<div class="sponsor sponsor-marginado">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/marginadoproducciones" alt="Marginado Producciones" ></a>
-						</div>
-						<!--
-						<div class="sponsor sponsor-mdpff">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.mardelplatafilmfest.com" alt="Mar del Plata Film Fest" ></a>
-						</div>
- 						<div class="sponsor sponsor-osde">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.artefundacionosde.com.ar" alt="Fundación OSDE - Espacio de Arte" ></a>
-						</div>
-						<div class="sponsor sponsor-picsi">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.facebook.com/Picsifotocabinas" alt="PICSI Fotocabinas" ></a>
-						</div>
- 						<div class="sponsor sponsor-purga">
-							<a target="_blank" rel="noopener noreferrer" href="https://purga.tv" alt="Purga" ></a>
-						</div>
- 						<div class="sponsor sponsor-manicomiodelterror">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/ManicomioFreakshow" alt="Manicomio Freakshow" ></a>
-						</div>
-						-->
- 						<div class="sponsor sponsor-guiar">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/asociaciondeguionistasarg" alt="Asociación de Guionistas Argentinos" ></a>
-						</div>
- 						<div class="sponsor sponsor-argentores">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.argentores.org" alt="Argentores - Sociedad General de Autores de la Argentina" ></a>
-						</div>
- 						<!--
- 						<div class="sponsor sponsor-embajada-espana">
-							<a target="_blank" rel="noopener noreferrer" href="http://cgeonline.com.ar" alt="Consulado General de España en Buenos Aires" ></a>
-						</div>
- 						<div class="sponsor sponsor-insomnio">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/insomnio.producciones" alt="Insomnio Producciones" ></a>
-						</div>
-						-->
- 						<div class="sponsor sponsor-cfyb">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.revistacfyb.com.ar" alt="Revista CFyB" ></a>
-						</div>
- 						<!--
- 						<div class="sponsor sponsor-fan">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.mixcloud.com.ar/FANelprograma" alt="FAN El Programa" ></a>
-						</div>
- 						<div class="sponsor sponsor-tvcortos">
-							<a target="_blank" rel="noopener noreferrer" href="https://shorts.tv/es" alt="TVCortos" ></a>
-						</div>
- 						<div class="sponsor sponsor-monster-flex">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/monsterlabsstudios" alt="Monster Flex" ></a>
-						</div>
-						-->
- 						<div class="sponsor sponsor-neftis">
-							<a target="_blank" rel="noopener noreferrer" href="http://elian.neftis.tv" alt="Neftis" ></a>
-						</div>
- 						<!--
- 						<div class="sponsor sponsor-global-media">
-							<a target="_blank" rel="noopener noreferrer" href="" alt="Global Media Distribution" ></a>
-						</div>
- 						<div class="sponsor sponsor-brujas">
-							<a target="_blank" rel="noopener noreferrer" href="http://www.disfracesbrujas.com.ar" alt="Disfraces Brujas Witchmaster" ></a>
-						</div>
-						-->
- 						<div class="sponsor sponsor-pintafan">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pintafancolorpro" alt="Pintafan Color Pro" ></a>
-						</div>
-						<!--
- 						<div class="sponsor sponsor-brigada-problematica">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/brigadaproblematica/" alt="Brigada Problemática" ></a>
-						</div>
- 						<div class="sponsor sponsor-okinawa-film-office">
-							<a target="_blank" rel="noopener noreferrer" href="https://filmoffice.ocvb.or.jp" alt="Okinawa Film Office" ></a>
-						</div>
- 						<div class="sponsor sponsor-felices-las-vacas">
-							<a target="_blank" rel="noopener noreferrer" href="http://feliceslasvacas.com" alt="Felices Las Vacas" ></a>
-						</div>
- 						<div class="sponsor sponsor-no-pain-tattoos">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/tattoosnopain/?igshid=yuvbb7fg9c5b" alt="No Pain Tattoos" ></a>
-						</div>
-						-->
- 						<div class="sponsor sponsor-yubari-festival">
-							<a target="_blank" rel="noopener noreferrer" href="https://yubarifanta.jp/" alt="Yubari Festival" ></a>
-						</div>
- 						<div class="sponsor sponsor-akataka">
-							<a target="_blank" rel="noopener noreferrer" href="https://akataka.com/" alt="Akataka" ></a>
-						</div>
- 						<div class="sponsor sponsor-enchulame-la-camara">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.enchulamerental.com/" alt="Enchulame la cámara" ></a>
-						</div>
- 						<div class="sponsor sponsor-rebisfx">
-							<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/RebisFXEfectosespeciales/" alt="Rebis FX" ></a>
-						</div>
-
-						<div class="sponsor sponsor-fant-latam"></div>
-					</div>
+								<div class="sponsors">
+									<?php
+ 									foreach($section['items'] as $sponsorItem) {
+ 										$logoPath = get_bloginfo('template_directory') . $sponsorItem['logo'];
+ 									?>
+ 										<div class="sponsor" style="background-image: url('<?php echo $logoPath ?>');">
+	 									<?php
+	 										$isClickableSponsor = isset($sponsorItem['href']) && !empty($sponsorItem['href']);
+	 										if ($isClickableSponsor) {
+	 									?>
+	 											<a target="_blank" rel="noopener noreferrer" href="<?php echo $sponsorItem['href'] ?>" alt="<?php echo $sponsorItem['alt'] ?>" ></a>
+										<?php
+											}
+										?>
+									  </div>
+									<?php
+ 									}
+									?>
+								</div>
+					<?php
+							}
+						}
+					?>
 				</div>
 	<?php
 		get_sidebar();
