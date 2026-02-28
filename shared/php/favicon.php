7146a3b76819ebbce943f779e18869e1e65fa2cb
Andrés Mata Suárez <amatasuarez@gmail.com>
2026-02-27 23:00:22 -0300

    Fixed favicon dimensions so its picked up by search engines

--- shared/php/favicon.php
+++ shared/php/favicon.php
@@ -11,6 +11,11 @@
 function bars_favicon_tags() {
     $base = get_template_directory_uri() . '/resources/favicon/';
     ?>
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo $base; ?>android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $base; ?>favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="<?php echo $base; ?>android-icon-48x48.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base; ?>favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base; ?>favicon-16x16.png">
     <link rel="icon" href="<?php echo $base; ?>favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $base; ?>apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $base; ?>apple-icon-60x60.png">
@@ -21,10 +26,6 @@ function bars_favicon_tags() {
     <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $base; ?>apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $base; ?>apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base; ?>apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo $base; ?>android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base; ?>favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $base; ?>favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base; ?>favicon-16x16.png">
     <link rel="manifest" href="<?php echo $base; ?>manifest.json">
     <meta name="msapplication-TileColor" content="#0A0A0A">
     <meta name="msapplication-TileImage" content="<?php echo $base; ?>ms-icon-144x144.png">
